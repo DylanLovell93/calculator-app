e724bbfba572a13ca833163b592eddb0b7c05331
DylanLovell <arcanyne7@gmail.com>
2021-11-12 11:20:16 -0500

    buttonInfo added

---
+++ src/data/buttonInfo.js
@@ -0,0 +1,99 @@
+const buttonInfo = [
+  {
+    symbol: 'AC',
+    color: 'top',
+    func: 'clear',
+  },
+  {
+    symbol: '±',
+    color: 'top',
+    func: 'negPos',
+  },
+  {
+    symbol: '%',
+    color: 'top',
+    func: 'percent',
+  },
+  {
+    symbol: '÷',
+    color: 'oper',
+    func: 'divide',
+  },
+  {
+    symbol: 7,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 8,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 9,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 'X',
+    color: 'oper',
+    func: 'multiply',
+  },
+  {
+    symbol: 4,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 5,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 6,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: '-',
+    color: 'oper',
+    func: 'subtract',
+  },
+  {
+    symbol: 1,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 2,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 3,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: '+',
+    color: 'oper',
+    func: 'add',
+  },
+  {
+    symbol: 0,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: 9,
+    color: 'num',
+    func: 'input',
+  },
+  {
+    symbol: '=',
+    color: 'oper',
+    func: 'equal',
+  },
+];
+
+export default buttonInfo;
